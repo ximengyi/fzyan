94d1e58f676c3cdda6d571166922ec9372f2ad12
MengYi <1203669692@qq.com>
2018-12-04 19:35:35 +0800

    修复state不能更新问题

--- api/controllers/DeviceController.php
+++ api/controllers/DeviceController.php
@@ -19,21 +19,21 @@ class DeviceController extends ActiveController
 
     public function actionSearch()
     {
-
         $name =  Yii::$app->request->get('name');
         $weight =  Yii::$app->request->get('weight');
         $state  = Yii::$app->request->get('state');
 
         $device_one = Device::find()->where(['=','name',$name])->one();
 
-        $device_one->state =(int)(((90 - $state)/90)*100);
+        $device_one->state =(string)round((((90 - $state)/90)*100));
         if(!empty($device_one)){
             $reward_code = new RewardCode();
             $reward_code->content = uniqid();
             $reward_code->money = round((($weight/500) * $device_one->price));
             $reward_code->trash_id = $device_one->id;
             $reward_code->created_at = time();
-
+          //  $device_one->validate();
+            // var_dump($device_one->getErrors());die;
             if($reward_code->create_code($reward_code,$device_one)){
 
                 return ['status'=>200,'code'=>$reward_code->content,'money'=>$reward_code->money];
